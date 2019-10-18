d211e75667f71bbb96244a09f650c6955ab0048d
Atif Aziz <code@raboof.com>
2019-10-18 09:31:24 +0200

    Fix typo in header comment for Precedence

--- src/com/stuffwithstuff/bantam/Precedence.java
+++ src/com/stuffwithstuff/bantam/Precedence.java
@@ -1,7 +1,7 @@
 package com.stuffwithstuff.bantam;
 
 /**
- * Defines the different precendence levels used by the infix parsers. These
+ * Defines the different precedence levels used by the infix parsers. These
  * determine how a series of infix expressions will be grouped. For example,
  * "a + b * c - d" will be parsed as "(a + (b * c)) - d" because "*" has higher
  * precedence than "+" and "-". Here, bigger numbers mean higher precedence.
